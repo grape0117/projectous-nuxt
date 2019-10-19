cafa768ca07abaad7959bfb80f57c4f177461eea
NovoManu <operamike@mail.ru>
2019-10-19 19:19:43 +0300

    Format code

--- src/store/modules/lists/mutations.ts
+++ src/store/modules/lists/mutations.ts
@@ -1,38 +1,37 @@
 // @ts-ignore
 import { MutationTree } from 'vuex'
-import { IListsState, ITask, IList, IUserTask } from './types'
+import { IListsState, ITask } from './types'
 import {
   FETCH_TASKS,
   SET_TASKS_TO_LIST,
-  SET_LISTS, ADD_NEW_LIST,
-  SET_ALL_TASKS,
+  SET_LISTS,
+  ADD_NEW_LIST,
   ADD_NEW_TASK
 } from './mutations-types'
 
 export const mutations: MutationTree<IListsState> = {
-  [FETCH_TASKS](state: IListsState, payload: any) {
-    const sortableTasks = payload.userTasks
-      .map((userTask: IUserTask) => (
-        payload.allTasks.find((task: ITask) => (
-          userTask.task_id === task.id)
-        )
-      ))
-      .filter((task: ITask) => task.due_date)
-      .sort((a: ITask, b: ITask) => (
-        // @ts-ignore
-        new Date(a.due_date) - new Date(b.due_date)
-      ))
+  [FETCH_TASKS](state: IListsState, { userTasks, allTasks }: any) {
+    const sortableTasks = userTasks
+      .map(({ task_id }: any) => allTasks.find(({ id }: any) => task_id === id))
+      .filter(({ due_date }: any) => due_date)
+      .sort(
+        ({ due_date: a }: any, { due_date: b }: any) =>
+          (new Date(a) as any) - (new Date(b) as any)
+      )
 
-    state.lists = state.lists.map(list => list.name === 'tasks' ? { ...list, tasks: sortableTasks } : list)
+    state.lists = state.lists.map(list =>
+      list.name === 'tasks' ? { ...list, tasks: sortableTasks } : list
+    )
   },
-  [SET_TASKS_TO_LIST](state: IListsState, payload: any) {
-    const sortableTasks = payload.tasks
-      .sort((a: ITask, b: ITask) => (
-        // @ts-ignore
-        new Date(b.due_date) - new Date(a.due_date)
-      ))
+  [SET_TASKS_TO_LIST](state: IListsState, { tasks, listName }: any) {
+    const sortableTasks = tasks.sort(
+      ({ due_date: a }: any, { due_date: b }: any) =>
+        (new Date(a) as any) - (new Date(b) as any)
+    )
 
-    state.lists = state.lists.map(list => list.name === payload.listName ? { ...list, tasks: sortableTasks } : list)
+    state.lists = state.lists.map(list =>
+      list.name === listName ? { ...list, tasks: sortableTasks } : list
+    )
   },
   [SET_LISTS](state: IListsState, lists: any) {
     state.lists = lists
@@ -41,8 +40,9 @@ export const mutations: MutationTree<IListsState> = {
     state.lists = [...state.lists, { name: newNameList, tasks: [] }]
   },
   [ADD_NEW_TASK](state: IListsState, { listName, taskName, index }: any) {
-    const getRandomArbitrary = (min: number, max: number): number => Math.ceil(Math.random() * (max - min) + min)
-    const taskId = getRandomArbitrary(1000, 999999)
+    const getRandomArbitrary = (min: number, max: number): number =>
+      Math.ceil(Math.random() * (max - min) + min)
+    const taskId: number = getRandomArbitrary(1000, 999999)
 
     // @ts-ignore
     state.allTasks = [...state.allTasks, { id: taskId, title: taskName }]
@@ -51,8 +51,10 @@ export const mutations: MutationTree<IListsState> = {
       if (list.name === listName) {
         const listTasks = list.tasks
 
-        // @ts-ignore
-        listTasks.splice(index, 0, { task_id: taskId, id: getRandomArbitrary(1000, 999999) })
+        listTasks.splice(index, 0, {
+          task_id: taskId,
+          id: getRandomArbitrary(1000, 999999)
+        })
         return { ...list, tasks: listTasks }
       } else {
         return list
