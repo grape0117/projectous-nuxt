31270949cc88a056ceaa7e6186fb37de02098d50
NovoManu <operamike@mail.ru>
2019-10-25 17:18:38 +0300

    Added custom headers to http

--- src/http/AxiosHttp.ts
+++ src/http/AxiosHttp.ts
@@ -4,25 +4,40 @@ import { IHttp } from './types'
 
 export class AxiosHttp extends BaseHttp implements IHttp {
   public async fetch(url: string): Promise<any> {
-    const { data } = await axios.get(`${this.baseUrl}${url}`)
+    const { data } = await axios.get(`${this.baseUrl}${url}`, {
+      headers: this.headers
+    })
     return data
   }
   public async get(url: string, id: number | string) {
     const { data } = await axios.get(`${this.baseUrl}${url}`, {
-      params: { id }
+      params: { id },
+      headers: this.headers
     })
     return data
   }
   public async post(url: string, data: any) {
-    const { data: response } = await axios.post(`${this.baseUrl}${url}`, {
-      data
-    })
+    const { data: response } = await axios.post(
+      `${this.baseUrl}${url}`,
+      { data },
+      {
+        headers: this.headers
+      }
+    )
     return response
   }
   public async put(url: string, id: number | string, data: any) {
-    return await axios.put(`${this.baseUrl}${url}/${id}`, { data })
+    return await axios.put(
+      `${this.baseUrl}${url}/${id}`,
+      { data },
+      {
+        headers: this.headers
+      }
+    )
   }
   public async delete(url: string, id: number | string) {
-    return await axios.delete(`${this.baseUrl}${url}/${id}`)
+    return await axios.delete(`${this.baseUrl}${url}/${id}`, {
+      headers: this.headers
+    })
   }
 }
--- src/http/BaseHttp.ts
+++ src/http/BaseHttp.ts
@@ -2,6 +2,10 @@ import { IHttp } from '@/http/types'
 
 export class BaseHttp implements IHttp {
   protected baseUrl: string = 'https://release.projectous.com/'
+  protected headers: any = {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Access-Control-Allow-Origin': '*'
+  }
   public async fetch(url: string): Promise<any> {
     this.throwError()
   }
