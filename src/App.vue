a4b0b5f98d72ac59f7d2af28cc7528109ce4aa46
NovoManu <operamike@mail.ru>
2020-02-15 15:37:23 +0200

    ToDo

--- src/App.vue
+++ src/App.vue
@@ -48,6 +48,7 @@ export default {
   },
   methods: {
     async getAppDataFromApi() {
+      // ToDo: error handling
       return await this.$http().get('/test-tasks')
     },
     async getAppData() {
