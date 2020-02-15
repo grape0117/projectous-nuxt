448ea29140f3b5491f6b91c4904b8ad17c9a07bb
NovoManu <operamike@mail.ru>
2020-02-15 14:51:36 +0200

    updated DB naming

--- src/plugins/idb.ts
+++ src/plugins/idb.ts
@@ -1,25 +1,25 @@
 import { openDB } from 'idb'
 
-const dbPromise = openDB('keyval-store', 1, {
+const dbPromise = openDB('application-data', 1, {
   upgrade(db) {
-    db.createObjectStore('keyval')
+    db.createObjectStore('all-data')
   }
 })
 
 export const idbKeyval = {
   async get(key: any) {
-    return (await dbPromise).get('keyval', key)
+    return (await dbPromise).get('all-data', key)
   },
   async set(key: any, val: any) {
-    return (await dbPromise).put('keyval', val, key)
+    return (await dbPromise).put('all-data', val, key)
   },
   async delete(key: any) {
-    return (await dbPromise).delete('keyval', key)
+    return (await dbPromise).delete('all-data', key)
   },
   async clear() {
-    return (await dbPromise).clear('keyval')
+    return (await dbPromise).clear('all-data')
   },
   async keys() {
-    return (await dbPromise).getAllKeys('keyval')
+    return (await dbPromise).getAllKeys('all-data')
   }
 }
