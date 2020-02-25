9677ee0d6b01205c83818cab5c0fe25c1e1501ca
NovoManu <operamike@mail.ru>
2020-02-25 15:07:58 +0200

    store for each entity type

--- src/plugins/idb.ts
+++ src/plugins/idb.ts
@@ -2,24 +2,31 @@ import { openDB } from 'idb'
 
 const dbPromise = openDB('application-data', 1, {
   upgrade(db) {
-    db.createObjectStore('all-data')
+    db.createObjectStore('company_clients')
+    db.createObjectStore('company_users')
+    db.createObjectStore('projects')
+    db.createObjectStore('project_users')
+    db.createObjectStore('task_users')
+    db.createObjectStore('tasks')
+    db.createObjectStore('user_task_lists')
+    db.createObjectStore('timers')
   }
 })
 
 export const idbKeyval = {
-  async get(key: any) {
-    return (await dbPromise).get('all-data', key)
+  async get(key: any, storeName: string) {
+    return (await dbPromise).get(storeName, key)
   },
-  async set(key: any, val: any) {
-    return (await dbPromise).put('all-data', val, key)
+  async set(key: any, val: any, storeName: string) {
+    return (await dbPromise).put(storeName, val, key)
   },
-  async delete(key: any) {
-    return (await dbPromise).delete('all-data', key)
+  async delete(key: any, storeName: string) {
+    return (await dbPromise).delete(storeName, key)
   },
-  async clear() {
-    return (await dbPromise).clear('all-data')
+  async clear(storeName: string) {
+    return (await dbPromise).clear(storeName)
   },
-  async keys() {
-    return (await dbPromise).getAllKeys('all-data')
+  async keys(storeName: string) {
+    return (await dbPromise).getAllKeys(storeName)
   }
 }
