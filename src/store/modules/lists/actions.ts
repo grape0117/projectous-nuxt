49df37ae6f0f4d34e80826ec8f9befb407c4fe93
NovoManu <operamike@mail.ru>
2020-02-25 16:49:07 +0200

    user_task_lists

--- src/store/modules/lists/actions.ts
+++ src/store/modules/lists/actions.ts
@@ -5,6 +5,11 @@ import { ADD_NEW_LIST } from '@/store/modules/lists/mutations-types'
 
 export const actions: ActionTree<IListsState, IRootState> = {
   async createList({ commit }, newList) {
+    await commit(
+      'updateIndexDBEntity',
+      { module: 'user_task_lists', value: newList },
+      { root: true }
+    )
     // Todo: @stephane - add new endpoint to create list
     commit(ADD_NEW_LIST, newList)
   }
