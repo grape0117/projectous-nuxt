bbdda411c87edb037c2e9ce901a5ff0c29b08eab
NovoManu <operamike@mail.ru>
2020-02-27 13:47:24 +0200

    modules-names

---
+++ src/store/modules-names.ts
@@ -0,0 +1,12 @@
+export const modulesNames: any = {
+  TASKS: 'tasks',
+  TASK_USERS: 'task_users',
+  COMPANY_clients: 'company_clients',
+  COMPANY_USERS: 'company_users',
+  PROJECTS: 'projects',
+  PROJECT_USERS: 'project_users',
+  USER_TASKS_LIST: 'user_task_lists',
+  TIMERS: 'timers'
+}
+
+export const modulesList = Object.values(modulesNames)
