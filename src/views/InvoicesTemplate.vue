15a4a815ca98dffde066d23a77b37d8c093a8846
Kristoffer Gonzaga <kristoffer.gonzaga@urios.edu.ph>
2021-03-16 13:03:47 +0800

    Fixed the table design

--- src/views/InvoicesTemplate.vue
+++ src/views/InvoicesTemplate.vue
@@ -19,7 +19,7 @@
       </pre> -->
     </div>
     <div class="tab-content">
-      <div role="tabpanel" id="open" class="tab-pane active">
+      <div class="invoices-table tab-pane active" role="tabpanel" id="open">
         <div class="invoices-header">
           <span>Status</span>
           <span>Date Created</span>
@@ -30,11 +30,11 @@
           <span>End Date</span>
           <span>Options</span>
         </div>
-        <div class="invoices-items" v-for="invoice in openinvoices()" :key="invoice.id">
+        <div v-for="invoice in openinvoices()" :key="invoice.id">
           <invoices-row v-bind:invoice="invoice" />
         </div>
       </div>
-      <div role="tabpanel" id="closed" class="tab-pane">
+      <div class="tab-pane invoices-table" role="tabpanel" id="closed">
         <div class="table-responsive">
           <div class="invoices-header">
             <span>Status</span>
@@ -46,7 +46,7 @@
             <span>End Date</span>
             <span>Options</span>
           </div>
-          <div class="invoices-items" v-for="invoice in closedinvoices()" :key="invoice.id">
+          <div v-for="invoice in closedinvoices()" :key="invoice.id">
             <invoices-row v-bind:invoice="invoice" />
           </div>
         </div>
@@ -110,5 +110,125 @@ export default {
 <style lang="scss" scoped>
 .row-date {
   border: 10px solid red !important;
+}
+</style>
+
+<style lang="scss">
+.invoices-table {
+  .invoices-header {
+    background-color: rgba($color: #000000, $alpha: 0.6);
+    color: white;
+    height: 50px;
+    font-size: 16px;
+    font-weight: 500;
+    padding: 10px 5px;
+    margin-bottom: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border-radius: 4px;
+    box-shadow: 0px 0px 8px rgba($color: #fff, $alpha: 1);
+
+    span:nth-child(1) {
+      width: 100%;
+      max-width: calc(100vw / 6);
+    }
+    span:nth-child(2) {
+      width: 100%;
+      max-width: calc(100vw / 12);
+    }
+    span:nth-child(3) {
+      width: 100%;
+      max-width: calc(100vw / 6);
+      min-width: 100px;
+    }
+    span:nth-child(4) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    span:nth-child(5) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    span:nth-child(6) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    span:nth-child(7) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    span:last-child {
+      width: 100%;
+      max-width: calc(100vw / 6);
+      min-width: 320px;
+    }
+  }
+  .invoices-items {
+    display: flex;
+    justify-content: space-around;
+    background-color: rgba($color: #000000, $alpha: 0.6);
+    color: white;
+    word-break: break-word;
+    padding: 10px 5px;
+
+    &:hover {
+      background-color: rgba($color: #000000, $alpha: 0.7);
+    }
+
+    > div:nth-child(1) {
+      width: 100%;
+      max-width: calc(100vw / 6);
+      // flex-grow: 10;
+    }
+    > div:nth-child(2) {
+      width: 100%;
+      max-width: calc(100vw / 12);
+    }
+    > div:nth-child(3) {
+      width: 100%;
+      max-width: calc(100vw / 6);
+      min-width: 100px;
+    }
+    > div:nth-child(4) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    > div:nth-child(5) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    > div:nth-child(6) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    > div:nth-child(7) {
+      width: 100%;
+      max-width: calc(100vw / 14);
+    }
+    > div:last-child {
+      width: 100%;
+      max-width: calc(100vw / 6);
+      min-width: 320px;
+      display: flex;
+      // border: 1px solid red;
+      justify-content: space-between;
+
+      .options {
+        // display: flex;
+        height: 38px;
+        // border: 1px solid red;
+        a:not(:last-child) {
+          margin-right: 5px;
+        }
+        // justify-content: space-around;
+      }
+      .payment {
+        // border: 1px solid red;
+        height: 38px;
+        width: 87px;
+      }
+    }
+  }
 }
 </style>
