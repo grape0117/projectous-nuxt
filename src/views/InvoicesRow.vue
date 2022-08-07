b7a3938c23e77780d1fb22afc559369ff9d56f8a
stephanedeuvaert <stephane@techaround.com>
2022-08-06 18:16:40 -0700

    fixing invoice days

--- src/views/InvoicesRow.vue
+++ src/views/InvoicesRow.vue
@@ -67,7 +67,7 @@ export default {
         const today = moment().startOf('day')
 
         //Difference in number of days
-        return moment.duration(today.diff(invoice_date)).asDays()
+        return Math.Floor(moment.duration(today.diff(invoice_date)).asDays()) + ' Days'
       } else {
         return null
       }
