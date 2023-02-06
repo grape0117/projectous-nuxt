798c72ebb963df5970ca99be2f572955518efa9f
jonathanemanuel211 <jonathanemanuel211@outlook.com>
2023-02-06 02:34:15 -0300

    fix task not removed from the view when delete a task

--- src/store/modules/tasks/mutations.ts
+++ src/store/modules/tasks/mutations.ts
@@ -9,6 +9,48 @@ export const mutations: MutationTree<IModuleState> = {
     // @ts-ignore
     this.commit('task_users/deleteByTaskId', entity.id) //TODO: handle here?
   },
+  DELETE(state: IModuleState, entity) {
+    if (state.completed_tasks.length > 0) {
+      // @ts-ignore
+      state.completed_tasks = state.completed_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.hold_tasks.length > 0) {
+      // @ts-ignore
+      state.hold_tasks = state.hold_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.active_tasks.length > 0) {
+      // @ts-ignore
+      state.active_tasks = state.active_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.when_possible_tasks.length > 0) {
+      // @ts-ignore
+      state.when_possible_tasks = state.when_possible_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.this_week_tasks.length > 0) {
+      // @ts-ignore
+      state.this_week_tasks = state.this_week_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.today_tasks.length > 0) {
+      // @ts-ignore
+      state.today_tasks = state.today_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.my_tasks.length > 0) {
+      // @ts-ignore
+      state.my_tasks = state.my_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.past_due_tasks.length > 0) {
+      // @ts-ignore
+      state.past_due_tasks = state.past_due_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.others_tasks.length > 0) {
+      // @ts-ignore
+      state.others_tasks = state.others_tasks.filter(({ id }) => id !== entity.id)
+    }
+    if (state.all_tasks.length > 0) {
+      // @ts-ignore
+      state.all_tasks = state.all_tasks.filter(({ id }) => id !== entity.id)
+    }
+  },
   deleteByProjectId(state: IModuleState, project_id) {
     state.tasks.forEach(task => {
       if (task.project_id === project_id) {
