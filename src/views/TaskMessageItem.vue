6f49d918e982ca338b8692d03a913a9debc103c6
Akihiro <supersoftdev123@gmail.com>
2023-03-27 20:08:38 -0700

    Click on file downloads the file

--- src/views/TaskMessageItem.vue
+++ src/views/TaskMessageItem.vue
@@ -3,7 +3,7 @@
     <b-avatar v-if="!is_me" class="mr-1 mb-4" :text="user_name" v-b-tooltip.hover :title="message.user.name" size="25px" />
     <div>
       <div class="message-wrapper">
-        <div class="message-container">
+        <div class="message-container" v-on:mouseover="showDownloadBtn = true" v-on:mouseleave="showDownloadBtn = false">
           <button v-if="message.thumbnail" @click="openImage" role="button" title="Open image" aria-label="Open image" aria-disabled="false" tabindex="-1" style="position: relative; display: flex; flex-direction: column; flex-grow: 0; flex-shrink: 0; overflow: hidden; align-items: center; justify-content: center; app-region: no-drag; background-color: transparent; border-color: transparent; text-align: left; border-width: 0px; width: 299.995px; height: 259.938px; border-radius: 0px 10px 10px; padding: 0px; cursor: pointer; border-style: solid;">
             <div role="none" style="position: relative; display: flex; flex-direction: column; flex-grow: 0; flex-shrink: 0; overflow: hidden; align-items: stretch; justify-content: center; border-radius: 0px 10px 10px; width: 299.995px; height: 259.938px;">
               <img v-if="!image_loaded" src="@/assets/img/no-image.png" width="300" height="260" alt="thumbnail" style="position: absolute;" />
@@ -11,6 +11,9 @@
             </div>
           </button>
           <img v-if="!message.thumbnail && message.isFile" src="@/assets/img/attach-file.png" width="200" height="220" alt="thumbnail" />
+          <div class="download-bg" v-if="!message.thumbnail && message.isFile && showDownloadBtn">
+            <i class="icon-download" @click="downloadFile(message)" />
+          </div>
           <pre v-if="!message.thumbnail" class="msg-content" style="color: white;">{{ message.message }}</pre>
         </div>
         <div class="message-actions" v-if="current_company_user_id == message.company_user_id">
@@ -46,7 +49,8 @@ export default {
   data() {
     return {
       open_actions: false,
-      image_loaded: false
+      image_loaded: false,
+      showDownloadBtn: false
     }
   },
   props: ['message', 'is_me'],
@@ -67,7 +71,11 @@ export default {
   },
   methods: {
     openImage() {
-      window.open('https://projectous-chat-bucket.sfo3.digitaloceanspaces.com/' + this.message.filePath, '_blank')
+      var image = new Image()
+      image.src = 'https://projectous-chat-bucket.sfo3.digitaloceanspaces.com/' + this.message.filePath
+      var w = window.open('', '_blank')
+      w.document.write(image.outerHTML)
+      w.document.close()
     },
     imgLoaded() {
       this.image_loaded = true
@@ -146,6 +154,26 @@ export default {
   display: flex;
   justify-content: space-between;
   // position: relative;
+
+  .message-container {
+    background-color: gray;
+    .download-bg {
+      position: absolute;
+      top: 0;
+      height: 100%;
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.7);
+      display: flex;
+      .icon-download {
+        color: white;
+        margin: auto;
+        font-size: 108px;
+      }
+      .icon-download:hover {
+        cursor: pointer;
+      }
+    }
+  }
 
   .message-actions {
     visibility: hidden;
