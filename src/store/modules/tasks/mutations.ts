df374618ae6efa22fc9864ab0260826bebdd8848
Akihiro <supersoftdev123@gmail.com>
2023-04-28 08:51:21 -0700

    fix chats order

--- src/store/modules/tasks/mutations.ts
+++ src/store/modules/tasks/mutations.ts
@@ -125,15 +125,15 @@ export const mutations: MutationTree<IModuleState> = {
     })
   },
   updateChats(state: IModuleState, chats) {
-    for (const chat of chats) {
-      const chatIndex = state.chats.findIndex(({ thread_id }) => thread_id == chat.thread_id)
+    let restChats = []
+    for (const chat of state.chats) {
+      // @ts-ignore
+      const chatIndex = chats.findIndex(({ thread_id }) => thread_id == chat.thread_id)
       if (chatIndex < 0) {
-        state.chats = [...state.chats, chat]
-      } else {
-        state.chats[chatIndex] = chat
+        restChats.push(chat)
       }
     }
-    state.chats = [...state.chats]
+    state.chats = [...chats, ...restChats]
   }
   /*,
   uuid_to_id(state: IModuleState, { uuid, id }) {
